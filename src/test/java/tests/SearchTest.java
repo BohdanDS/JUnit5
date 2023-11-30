77bfc9c218249a593ee51bc69335d18f9bed883d
bohdan_peliutkevich <bohdanpeliutkevich@gmail.com>
2023-11-30 12:13:12 +0300

    Adding allure

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,6 +1,8 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.Queries;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,8 +18,11 @@ import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
 
 public class SearchTest extends TestBase {
+
 
     static Queries iPhone = Queries.IPHONE;
     static Queries macBook = Queries.MAC;
@@ -25,9 +30,10 @@ public class SearchTest extends TestBase {
 
     @BeforeEach
     void setUp() {
-        open("https://www.apple.com/");
 
-        $("#globalnav-menubutton-link-search").click();
+        step("Открываем главную страницу", () -> open("https://www.apple.com/"));
+
+        step("Нажимаем на иконку поиска", () -> $("#globalnav-menubutton-link-search").click());
     }
 
     @ValueSource(strings = {"Iphone 15", "AirPods", "Macbook"})
@@ -38,17 +44,28 @@ public class SearchTest extends TestBase {
     })
     void verifySearchReturnsNotEmptyResultsTest(String searchQuery) {
 
-        $("[placeholder='Search apple.com']").setValue(searchQuery).pressEnter();
-        $$("#exploreCurated").shouldBe(sizeGreaterThan(0));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("В поле поиска вводим значение: " + searchQuery, () -> {
+            $("[placeholder='Search apple.com']").setValue(searchQuery).pressEnter();
+            attachment("Source", webdriver().driver().source());
+        });
+        step("Проверяем что отображаются ответы", () -> {
+            $$("#exploreCurated").shouldBe(sizeGreaterThan(0));
+        });
+
     }
 
     @CsvFileSource(resources = "/test_data/verifySearchReturnsCorrectResultTest.csv", delimiter = '|')
-    @ParameterizedTest(name="Проверка первого результата {1} по запросу {0}")
+    @ParameterizedTest(name = "Проверка первого результата {1} по запросу {0}")
     @Tags({
             @Tag("SMOKE"),
             @Tag("WEB"),
     })
-    void verifyFirstSearchResultTest(String searchQuery, String searchResult){
+    void verifyFirstSearchResultTest(String searchQuery, String searchResult) {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         $("[placeholder='Search apple.com']").setValue(searchQuery).pressEnter();
         $(".rf-serp-explore-curated-position-1 h2").shouldHave(text(searchResult));
     }
@@ -69,6 +86,7 @@ public class SearchTest extends TestBase {
                 )
         );
     }
+
     @MethodSource("searchQueriesAndResults")
     @Tags({
             @Tag("CRITICAL"),
@@ -76,6 +94,9 @@ public class SearchTest extends TestBase {
     })
     @ParameterizedTest(name = "Поиск по запросу {0} и сравнение результатов {1}")
     void verifySearchResultsTest(String searchQuery, List<String> searchResults) {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         $("[placeholder='Search apple.com']").setValue(searchQuery).pressEnter();
         $$("#exploreCurated h2").filter(visible).shouldHave(texts(searchResults));
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
+
     @BeforeAll
     static void beforeFillForm(){
         Configuration.browserSize = "1920x1080";
